126b2fc54a0f9a06ca10e35f3adefc14dff2899a
dionnijssen <dionnijssen@hotmail.com>
2023-06-12 00:28:42 +0200

    refactor: cleanup of models

--- Logic/Models/Order.java
+++ Logic/Models/Order.java
@@ -1,16 +1,13 @@
 package Logic.Models;
 
-import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class Order {
     private int id;
     private ArrayList<Orderrule> orderrules;
-    private LocalDate date;
 
-    public Order(int id, LocalDate date, ArrayList<Orderrule> orderrules) {
+    public Order(int id, ArrayList<Orderrule> orderrules) {
         this.id = id;
-        this.date = date;
         this.orderrules = orderrules;
     }
 
--- Logic/Models/Orderrule.java
+++ Logic/Models/Orderrule.java
@@ -2,17 +2,12 @@ package Logic.Models;
 
 public class Orderrule {
 
-    private int id;
     private Article article;
     private int amount;
 
     public Orderrule(Article article, int amount) {
         this.article = article;
         this.amount = amount;
-    }
-
-    public int getId() {
-        return this.id;
     }
 
     public Article getArticle() {
