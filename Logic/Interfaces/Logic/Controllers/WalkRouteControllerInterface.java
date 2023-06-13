f55a78bb8f877cc2375b4ce6a14ce39822eb5efc
dionnijssen <dionnijssen@hotmail.com>
2023-06-13 18:14:28 +0200

    refactor: updated interfaces

--- Logic/Interfaces/Logic/Controllers/WalkRouteControllerInterface.java
+++ Logic/Interfaces/Logic/Controllers/WalkRouteControllerInterface.java
@@ -1,7 +1,11 @@
 package Logic.Interfaces.Logic.Controllers;
 
+import Logic.Models.Article;
 import Logic.Models.WalkRoute;
+
+import java.util.ArrayList;
 
 public interface WalkRouteControllerInterface {
     WalkRoute show(int id);
+    WalkRoute create(ArrayList<Article> articles);
 }
